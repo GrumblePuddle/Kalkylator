6d4299cf7bb2a6c9738da06f5b46a61df18ad048
Gustav Liffner <gurra_liffner@hotmail.com>
2024-12-05 00:15:55 +0000

    First UI design

--- kalkylator-app/src/App.jsx
+++ kalkylator-app/src/App.jsx
@@ -1,35 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="kalkylator">
+      <div className="input-container">
+        <input type="text" className="input" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="container">
+        <div className=" item item-1">
+          <button>CE</button>
+        </div>
+        <div className=" item item-2">
+          <button>%</button>
+        </div>
+        <div className="item item-3">
+          <button>7</button>
+        </div>
+        <div className="item item-4">
+          <button>8</button>
+        </div>
+        <div className="item item-5">
+          <button>9</button>
+        </div>
+        <div className="item item-6">
+          <button>/</button>
+        </div>
+        <div className="item item-7">
+          <button>4</button>
+        </div>
+        <div className="item item-8">
+          <button>5</button>
+        </div>
+        <div className="item item-9">
+          <button>6</button>
+        </div>
+        <div className="item item-10">
+          <button>x</button>
+        </div>
+        <div className="item item-11">
+          <button>1</button>
+        </div>
+        <div className="item item-12">
+          <button>2</button>
+        </div>
+        <div className="item item-13">
+          <button>3</button>
+        </div>
+        <div className="item item-14">
+          <button>-</button>
+        </div>
+        <div className="item item-15">
+          <button>0</button>
+        </div>
+        <div className="item item-16">
+          <button>.</button>
+        </div>
+        <div className="item item-17">
+          <button>=</button>
+        </div>
+        <div className="item item-18">
+          <button>+</button>
+        </div>
+        <div className="item item-19">
+          <button>ON/C</button>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
